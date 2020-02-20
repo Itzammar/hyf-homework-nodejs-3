b5026f0bcf9403ad0cc64f70e17490717597c22a
Gabriela Orozco <gsoto@unitedvirtualities.com>
2020-02-20 16:15:42 -0700

    GET request at /users working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+var users = []
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
+});
+
+app.get('/users', (req, res) => {
+    res.send(users);
 })
 
 app.listen(3000,function(){
     console.log("server running")
-})
+});
